d54d67de3b56f7a3dd561fd8edb557d88bc1dfa6
MarcosD00 <olito007.23@gmail.com>
2025-01-11 20:06:12 -0600

    adding to the branch

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function AboutUs() {
     return (
     <main>
         <h1>About</h1>
+        <p><Link href="/">Home</Link></p>
     </main>
     )
 }
